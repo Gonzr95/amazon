26c19ebbccf2207c99b8f2998d1e1445b4e6e53a
Gonzalo Rodriguez TrenesArgentinos <gonz.r@hotmail.com>
2025-03-18 10:22:00 -0300

    Se genera HTML para las opciones de entrega

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,18 @@
 
 
-export const cart = JSON.parse(localStorage.getItem('cart'));
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
 if(!cart){
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
     },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '3'
     }];
-}
+  }
 
 export function addToCart(productId){
     let matchingProduct;
@@ -17,14 +20,16 @@ export function addToCart(productId){
       if(productId === cartItem.productId){
         matchingProduct = cartItem;
       }
-    });
+    }); 
     if(matchingProduct){
       matchingProduct.quantity += 1;
     }
     else{
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
+
       });
     }
     saveToStorage();
